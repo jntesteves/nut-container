2a012042f90ee45b91931e35e63d80a7b58372b7
Jonatas Esteves <jntesteves@gmail.com>
2024-01-07 11:59:55 -0300

    Truncate titledb/files.json to avoid initialization errors
    NUT can't deal with ROMs being deleted and errors out on initialization. Truncate the ROMs list and re-scan every time the container is launched to avoid the issue.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 [ "$USERNAME" ] && [ "$PASSWORD" ] && printf '%s|%s\n' "$USERNAME" "$PASSWORD" >>/app/conf/users.conf && :
 
 if [ ! "$NO_SCAN" ]; then
+	printf '[]\n' >/app/titledb/files.json
 	python /app/nut.py --scan
 	/nut-scan-daemon.sh &
 fi
